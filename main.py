60a49fbf1ec53b02e7628be176a1eb4e659f5cb1
tamilmoviesdownload <ajayneymar14@gmail.com>
2026-04-02 18:54:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # --- CONFIG ---
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-CHAT_ID = "-1002754017596"
+CHAT_ID = "-1003731149259"
 EARN4LINK_API_KEY = os.environ.get("EARN4LINK_API_KEY", "")
 DOWNLOAD_TUTORIAL_LINK = "https://t.me/howtodownloadtlink"
 
